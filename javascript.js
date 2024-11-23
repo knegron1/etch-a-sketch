92997bb1ec885158bc2d1083136ff85f6af3103c
Katherine Negron <katherinenegron98@gmail.com>
2024-11-22 17:52:34 -0800

    added event listeners that change the color of each div in grid if hovered over

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 const container = document.querySelector(".container");
 
+// creates a 16x16 div grid
 for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.classList.add("gridItems")
     container.appendChild(div);
 }
+
+// adds event listeners to each div in grid to change the color if hovered over
+const gridItems = document.querySelectorAll(".gridItems");
+gridItems.forEach((item, index) => {
+    item.addEventListener("mouseenter", () => item.style.backgroundColor = "blue" );
+    item.addEventListener("mouseleave", () => item.style.backgroundColor = "black");
+})
